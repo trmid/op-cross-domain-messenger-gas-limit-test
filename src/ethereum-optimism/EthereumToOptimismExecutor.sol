a442fb30cb8203e8532f4a768916e4d4f6899b28
Trevor Richard <trmidpoint@gmail.com>
2023-08-25 18:07:56 +0000

    add test

---
+++ src/ethereum-optimism/EthereumToOptimismExecutor.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.19;
+
+import { ICrossDomainMessenger } from "../interfaces/ICrossDomainMessenger.sol";
+
+import "../interfaces/IMessageExecutor.sol";
+import "../libraries/MessageLib.sol";
+
+/**
+ * @title MessageExecutorOptimism contract
+ * @notice The MessageExecutorOptimism contract executes messages from the Ethereum chain.
+ *         These messages are sent by the `MessageDispatcherOptimism` contract which lives on the Ethereum chain.
+ */
+contract MessageExecutorOptimism is IMessageExecutor {
+  /* ============ Variables ============ */
+
+  /// @notice Address of the Optimism cross domain messenger on the Optimism chain.
+  ICrossDomainMessenger public immutable crossDomainMessenger;
+
+  /// @notice Address of the dispatcher contract on the Ethereum chain.
+  IMessageDispatcher public dispatcher;
+
+  /**
+   * @notice Mapping to uniquely identify the messages that were executed
+   *         messageId => boolean
+   * @dev Ensure that messages cannot be replayed once they have been executed.
+   */
+  mapping(bytes32 => bool) public executed;
+
+  /* ============ Constructor ============ */
+
+  /**
+   * @notice MessageExecutorOptimism constructor.
+   * @param _crossDomainMessenger Address of the Optimism cross domain messenger on the Optimism chain
+   */
+  constructor(ICrossDomainMessenger _crossDomainMessenger) {
+    require(address(_crossDomainMessenger) != address(0), "Executor/CDM-not-zero-address");
+    crossDomainMessenger = _crossDomainMessenger;
+  }
+
+  /* ============ External Functions ============ */
+
+  /// @inheritdoc IMessageExecutor
+  function executeMessage(
+    address _to,
+    bytes calldata _data,
+    bytes32 _messageId,
+    uint256 _fromChainId,
+    address _from
+  ) external {
+    IMessageDispatcher _dispatcher = dispatcher;
+    _isAuthorized(_dispatcher);
+
+    bool _executedMessageId = executed[_messageId];
+    executed[_messageId] = true;
+
+    MessageLib.executeMessage(_to, _data, _messageId, _fromChainId, _from, _executedMessageId);
+
+    emit MessageIdExecuted(_fromChainId, _messageId);
+  }
+
+  /// @inheritdoc IMessageExecutor
+  function executeMessageBatch(
+    MessageLib.Message[] calldata _messages,
+    bytes32 _messageId,
+    uint256 _fromChainId,
+    address _from
+  ) external {
+    IMessageDispatcher _dispatcher = dispatcher;
+    _isAuthorized(_dispatcher);
+
+    bool _executedMessageId = executed[_messageId];
+    executed[_messageId] = true;
+
+    MessageLib.executeMessageBatch(_messages, _messageId, _fromChainId, _from, _executedMessageId);
+
+    emit MessageIdExecuted(_fromChainId, _messageId);
+  }
+
+  /**
+   * @notice Set dispatcher contract address.
+   * @dev Will revert if it has already been set.
+   * @param _dispatcher Address of the dispatcher contract on the Ethereum chain
+   */
+  function setDispatcher(IMessageDispatcher _dispatcher) external {
+    require(address(dispatcher) == address(0), "Executor/dispatcher-already-set");
+    dispatcher = _dispatcher;
+  }
+
+  /* ============ Internal Functions ============ */
+
+  /**
+   * @notice Check if sender is authorized to message `executeMessageBatch`.
+   * @param _dispatcher Address of the dispatcher on the Ethereum chain
+   */
+  function _isAuthorized(IMessageDispatcher _dispatcher) internal view {
+    ICrossDomainMessenger _crossDomainMessenger = crossDomainMessenger;
+
+    require(
+      msg.sender == address(_crossDomainMessenger) &&
+        _crossDomainMessenger.xDomainMessageSender() == address(_dispatcher),
+      "Executor/sender-unauthorized"
+    );
+  }
+}
--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,42 +1,61 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
-import { console2 } from "forge-std/console2.sol";
+import "forge-std/console2.sol";
 
-import { Foo } from "../src/Foo.sol";
+import { ICrossDomainMessenger } from "../src/interfaces/ICrossDomainMessenger.sol";
+import { IMessageExecutor } from "../src/interfaces/IMessageExecutor.sol";
+import { MessageDispatcherOptimism } from "../src/ethereum-optimism/EthereumToOptimismDispatcher.sol";
 
-interface IERC20 {
-  function balanceOf(address account) external view returns (uint256);
-}
+contract TestMessageDispatcherOptimism is Test {
 
-/// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
-/// https://book.getfoundry.sh/forge/writing-tests
-contract FooTest is Test {
+  /* ============ Global Variables ============ */
   uint256 public mainnetFork;
 
-  Foo public fooContract = new Foo();
+  address public crossDomainMessenger = 0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1;
 
+  MessageDispatcherOptimism public dispatcher;
+
+  uint256 recentMainnetBlock = 17993284;
+  uint256 blockBeforeBedrockUpgrade = 16473770; // use this for fork to see how it use to not affect the gas usage on L1
+
+  /* ============ Set Up ============ */
   function setUp() public {
-    mainnetFork = vm.createFork(vm.rpcUrl("mainnet"), 16_428_000);
+    mainnetFork = vm.createFork(vm.rpcUrl("mainnet"), 17993284);
   }
 
-  /// @dev Simple test. Run Forge with `-vvvv` to see console logs.
-  function test() external {
-    string memory foo = fooContract.getFoo();
-    console2.log(foo);
+  /* ============ test ============ */
+  function testForkGasLimit() external {
+    useMainnet();
 
-    assertEq(foo, "Foo");
+    uint256 MAX_GAS_LIMIT = 1_920_000;
+    uint256 MIN_GAS_LIMIT = 0;
+    bytes memory _calldata = bytes("some decently long calldata that is invalid, but should be just fine unless there are some actual pre-checks on calldata to make sure it executes.");
+
+    // call dispatchMessage once before to reduce gas from any initialization of storage, .etc
+    dispatcher.dispatchMessage(5, address(2), MAX_GAS_LIMIT, _calldata);
+
+    uint8 iterations = 10;
+    for (uint i = iterations; i > 0; i--) {
+      uint256 _gasLimit = (MAX_GAS_LIMIT * i) / iterations;
+      if (_gasLimit >= MIN_GAS_LIMIT) {
+        vm.roll(block.number + 1); // make sure we aren't increasing fees artificially by submitting multiple calls per block
+        uint256 gas0 = gasleft();
+        dispatcher.dispatchMessage(5, address(2), _gasLimit, _calldata);
+        uint256 gas1 = gasleft();
+        console2.log("%s gasLimit, L1 gasConsumed: %s", _gasLimit, gas0 - gas1);
+      }
+    }
   }
 
-  /// @dev Test that runs against a fork of Ethereum Mainnet. You need to set `MAINNET_RPC_URL` in your `.envrc`
-  function testFork() external {
+  /* ============ Helpers ============ */
+
+  /// @dev Run at the beginning of each fork test
+  function useMainnet() public {
     vm.selectFork(mainnetFork);
-
-    address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
-    uint256 actualBalance = IERC20(usdc).balanceOf(holder);
-    uint256 expectedBalance = 196_307_713.810457e6;
-    assertEq(actualBalance, expectedBalance);
+    dispatcher = new MessageDispatcherOptimism(ICrossDomainMessenger(crossDomainMessenger), 5);
+    dispatcher.setExecutor(IMessageExecutor(address(1)));
   }
+
 }
